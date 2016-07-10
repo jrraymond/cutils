6a708155f2481714f870c08a4c75db47199dda29
Justin Raymond <jraymond@wesleyan.edu>
2016-07-10 16:22:51 -0400

    .

--- unordered_set.h
+++ unordered_set.h
@@ -2,13 +2,26 @@
 #define __UNORDERED_SET_H
 
 #include <stdlib.h>
+#include <stdint.h>
 
+/* macros for checking flags. we need to keep track of 3 states: empty,
+ * occupied, deleted. This requires 2 bits. 00 -> empty, 01->occupied,
+ * 10->deleted. A flag is 8 bits so it holds 4 subflags.
+ */
+#define US_FLAG_EMP 0
+#define US_FLAG_OCC 1
+#define US_FLAG_DEL 2
+#define __us_is_empty(flag, i) (((flag >> (2*i)) & 1) == US_FLAG_EMP)
+#define __us_is_occ(flag, i) (((flag >> (2*i)) & 1) == US_FLAG_OCC)
+#define __us_is_del(flag, i) (((flag >> (2*i)) & 1) == US_FLAG_DEL)
+#define __us_set_empty(flag, i) (flag & (~((US_FLAG_EMP << (2*i)
 
 #define UNORDERED_SET_INIT(name, scope, elem_t, _hash_f, _eq_f)            \
   typedef struct {                                                         \
     size_t capacity;                                                       \
     size_t size;                                                           \
-    int *flags;                                                            \
+    uint8_t *flags;                                                        \
+    uint64_t *hash;                                                        \
     elem_t *elems;                                                         \
   } us_##name##_t ;                                                        \
                                                                            \
